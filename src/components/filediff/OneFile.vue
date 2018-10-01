9d4dd58ecd2cad05d2b320a9360765a0c3f10d29
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-10-01 09:14:06 -0400

    scrolling and dummy diff

--- src/components/filediff/OneFile.vue
+++ src/components/filediff/OneFile.vue
@@ -6,7 +6,11 @@
       {{ file.path }}
     </div>
     <div v-if="!isCollapsed" class="file-diff-container">
-      File Diff Contents go here
+      <pre
+        v-for="(row, rowIndex) in diffRows"
+        :key="'row' + rowIndex"
+        :class="rowClass(row)"
+      >{{ row }}</pre>
     </div>
   </div>
 </template>
@@ -29,8 +33,73 @@ export default {
   },
   data: function () {
     return {
-      isCollapsed: true
+      isCollapsed: true,
+      rawDiff: `diff --git a/src/views/FileDiff.vue b/src/views/FileDiff.vue
+index 2f9a826..213c167 100644
+--- a/src/views/FileDiff.vue
++++ b/src/views/FileDiff.vue
+@@ -19,12 +19,28 @@ export default {
+   data: function () {
+     return {
+       files: [
+-        {
+-          path: 'C:\\Folder'
+-        },
+-        {
+-          path: 'C:\\Users\\Bob\\GitHub\\Reponame'
+-        }
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
++        { path: 'C:\\Folder' },
++        { path: 'C:\\Users\\Bob\\GitHub\\Reponame\\asdf\\qwer\\dfg\\rttryuqrewv
+ca\\qwer\\asdfqwerfzxv\\qwer\\asvasvr\\ascaweawe\\cwacawsdca\\acwecawecasd\\qweq
+c\\sdfvs' }
+       ]
+     };
+   }`,
+      asdf: []
     };
+  },
+  methods: {
+    rowClass: function (row) {
+      let color = '';
+
+      if (row.startsWith('+')) {
+        color = 'green-diff';
+      } else if (row.startsWith('-')) {
+        color = 'red-diff';
+      }
+
+      let classes = [
+        'row-diff',
+        color
+      ].join(' ').trim();
+
+      return classes.trim();
+    }
+  },
+  computed: {
+    diffRows: function () {
+      return this.rawDiff.split('\n');
+    }
   }
 };
 </script>
--- src/views/FileDiff.vue
+++ src/views/FileDiff.vue
@@ -19,12 +19,28 @@ export default {
   data: function () {
     return {
       files: [
-        {
-          path: 'C:\\Folder'
-        },
-        {
-          path: 'C:\\Users\\Bob\\GitHub\\Reponame'
-        }
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame' },
+        { path: 'C:\\Folder' },
+        { path: 'C:\\Users\\Bob\\GitHub\\Reponame\\asdf\\qwer\\dfg\\rttryuqrewvca\\qwer\\asdfqwerfzxv\\qwer\\asvasvr\\ascaweawe\\cwacawsdca\\acwecawecasd\\qweqc\\sdfvs' }
       ]
     };
   }
