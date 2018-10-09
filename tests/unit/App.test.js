c175147b20255ef2b5b902665cd78a444903ff6e
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-10-08 22:16:06 -0400

    Lint

--- tests/unit/App.test.js
+++ tests/unit/App.test.js
@@ -4,7 +4,6 @@ import App from '@/App.vue';
 
 describe('App.vue', () => {
   test('Default snapshot', () => {
-    const msg = 'new message';
     const wrapper = shallowMount(App);
 
     expect(wrapper.html())
