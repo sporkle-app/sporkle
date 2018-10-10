2c542fdd397fbc40e1fb29d5e90c494efa16eb83
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-10-09 22:27:36 -0400

    About App

---
+++ src/views/AboutApp.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="about-app">
+    Karng Darbo is a Cross-Platform Git client made by
+    <strong><a href="#" title="http://TheJaredWilcurt.com" @click.prevent="openExternal">The Jared Wilcurt</a></strong>.
+
+    <ul>
+      <li><strong>Karng Darbo</strong> (v{{ versions.karngDarbo }})</li>
+      <li><strong>Git</strong> (v{{ versions.git }})</li>
+      <li><strong>NW.js</strong> (v{{ versions.nw }})
+        <ul>
+          <li><strong>Chromium</strong> (v{{ versions.chromium }})</li>
+          <li><strong>Node.js</strong> (v{{ versions.node }})</li>
+        </ul>
+      </li>
+      <li><strong>Vue.js</strong> (v{{ versions.vue | clean }})</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const exec = nw.require('child_process').exec;
+
+export default {
+  name: 'AppSettings',
+  data: function () {
+    return {
+      appTitle: nw.App.manifest.window.title,
+      versions: {
+        chromium: nw.process.versions.chromium,
+        git: '',
+        karngDarbo: nw.App.manifest.version,
+        node: nw.process.versions.node,
+        nw: nw.process.versions.nw,
+        vue: nw.App.manifest.dependencies.vue
+      }
+    };
+  },
+  methods: {
+    getGitVersion: function () {
+      exec('git --version', (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        this.versions.git = data.replace('git version ', '').trim();
+      });
+    },
+    openExternal: function (evt) {
+      let url = evt.target.title;
+      nw.Shell.openExternal(url);
+    }
+  },
+  filters: {
+    clean: function (val) {
+      return val.replace('^', '').replace('~', '').trim();
+    }
+  },
+  created: function () {
+    this.getGitVersion();
+  }
+};
+</script>
