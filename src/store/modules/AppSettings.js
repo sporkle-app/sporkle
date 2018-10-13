9b8231fee2c2ca02f1a780ef443602b1e210c4bd
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-10-13 14:53:22 -0400

    Fix load settings bug

--- src/store/modules/AppSettings.js
+++ src/store/modules/AppSettings.js
@@ -37,10 +37,9 @@ export const actions = {
   },
   loadSettings: function (store) {
     store.commit('setAppLoading', true, { root: true });
+    let settings = {};
 
     if (fs.existsSync(settingsFile)) {
-      let settings = {};
-
       fs.readFile(settingsFile, function (err, data) {
         if (err) {
           store.commit('setAppError', 'Unable to load settings.\n' + err, { root: true });
@@ -51,22 +50,21 @@ export const actions = {
             settings = JSON.parse(data);
           } catch (error) {
             store.commit('setAppError', 'Error attempting to load settings.\n' + error, { root: true });
-            return;
           }
         }
 
-        if (Object.keys(settings).length) {
-          // Apply Settings
-          store.commit('setCustomScrollbars', settings.customScrollbars);
-          store.commit('reposList/setReposList', settings.reposList, { root: true });
-          store.commit('setTheme', settings.theme);
-        }
-
-        store.commit('setAppLoading', false, { root: true });
+        store.dispatch('applySettings', settings);
       });
     } else {
-      store.commit('setAppLoading', false, { root: true });
+      store.dispatch('applySettings', settings);
     }
+  },
+  applySettings: function (store, settings) {
+    settings = settings || {};
+    store.commit('setCustomScrollbars', settings.customScrollbars);
+    store.commit('reposList/setReposList', settings.reposList, { root: true });
+    store.commit('setTheme', settings.theme);
+    store.commit('setAppLoading', false, { root: true });
   },
   saveSettings: function (store) {
     // Grab Settings
