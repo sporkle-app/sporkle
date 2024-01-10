68deb6c67e01bd6b3faba9ce6c49a8dcde1ee3a2
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2024-01-09 22:35:56 -0500

    cleanup

--- src/components/sidebar/SideBarControls.vue
+++ src/components/sidebar/SideBarControls.vue
@@ -25,27 +25,6 @@
         :to="{ name: 'about' }"
       />
     </div>
-
-    <BaseModal
-      v-if="showSettings"
-      title="Settings"
-      @close="showSettings = false"
-    >
-      <template #body>
-        <AppSettings />
-      </template>
-    </BaseModal>
-
-    <BaseModal
-      v-if="showAbout"
-      title="About"
-      @close="showAbout = false"
-    >
-      <template #body>
-        <AboutApp />
-      </template>
-    </BaseModal>
-
   </div>
 </template>
 
@@ -54,19 +33,10 @@ import { mapActions } from 'pinia';
 
 import { andSaveStore } from '@/stores/andSave.js';
 
-import BaseModal from '@/components/BaseModal.vue';
-import AboutApp from '@/views/AboutApp.vue';
-import AppSettings from '@/views/AppSettings.vue';
-
 const openFolderExplorer = window.require('nw-programmatic-folder-select');
 
 export default {
   name: 'SideBarControls',
-  components: {
-    AboutApp,
-    AppSettings,
-    BaseModal
-  },
   methods: {
     addRepo: function () {
       const title = 'Select your repo folder';
