8e9db0022e3281b15adf1dcbfbe78b98623ea054
sseidmed <sseidmed@gmail.com>
2024-01-14 16:59:14 -0500

    Add a rough outline for the timeline

--- src/components/timeline/TimeLine.vue
+++ src/components/timeline/TimeLine.vue
@@ -12,7 +12,117 @@
     </button>
     <strong>Branches:</strong> {{ branches }}
     <div>
-      Timeline
+      <svg width=100 height=100 style='overflow: visible;'>
+        <path d='M100,20 L705,20'
+          fill='none' stroke='white' stroke-width=5 transform='translate(50,0)' />
+        <path d='M100,20 Q130,20 160,45 T685,60'
+          fill='none' stroke='white' stroke-width=5 transform='translate(50,0)' />
+          M10 80 Q 77.5 10, 145 80 T 280 80
+        <circle
+          cx="520"
+          cy="20"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+
+        <circle
+          cx="300"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="340"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="380"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="420"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="460"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="500"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="540"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="580"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="620"
+          cy="60"
+          r="6"
+          stroke-width="6"
+          fill="#FFFFFF"
+          stroke="white"
+        />
+        <circle
+          cx="660"
+          cy="60"
+          r="8"
+          stroke-width="4"
+          stroke="white"
+        />
+        <circle
+          cx="700"
+          cy="60"
+          r="8"
+          stroke-width="4"
+          stroke="white"
+        />
+
+        <circle
+          cx="740"
+          cy="60"
+          r="12"
+          stroke-width="4"
+          stroke-dasharray="15 4"
+          stroke="white"
+        />
+      </svg>
     </div>
   </div>
 </template>
