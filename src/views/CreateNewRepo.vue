8c1fa1253d52590a0a30a27ddd45929487ec1fcb
Ania <aniakbennett@gmail.com>
2024-02-14 16:30:02 -0500

    fixed

--- src/views/CreateNewRepo.vue
+++ src/views/CreateNewRepo.vue
@@ -1,52 +1,59 @@
 <template>
   <ViewWrapper title="Create a new repository">
     <ReposFolderPicker />
-    <label for="repos-folder-input">
+    <label for="repoName">
       <strong>
         New repository Name:
       </strong>
     </label>
     <div class="new-repo">
       <input
+        v-model="fileName"
+        id="repoName"
         type="text"
-        id="nameRepo"
-        class="nameRepo"
       />
     </div>
-      <label>
-          <strong>
-            Will create new repository at this location:
-          </strong>
-        </label>
-      <div>{{ filePath }}</div>
-      <button @click="createNewRepo">
-        Create new repository
-      </button>
-
+      <strong>
+        Will create new repository at this location:
+      </strong>
+    <div>{{ repoPath }}</div>
+    <button @click="createNewRepo">
+      Create new repository
+    </button>
     
     
     <div>
-      
+      {{  reposFolder }}
     </div>
   </ViewWrapper>
 </template>
 
 <script>
-import ViewWrapper from '@/views/ViewWrapper.vue';
+import { mapState } from 'pinia';
 import ReposFolderPicker from '@/components/appsettings/ReposFolderPicker.vue';
+import ViewWrapper from '@/views/ViewWrapper.vue';
+import { reposStore } from '@/stores/repos.js';
 
 export default {
   name: 'CreateNewRepo',
   components: {
     ViewWrapper,
-    ReposFolderPicker,
+    ReposFolderPicker
   },
-  methods: {
-    updateFilePath() {
+  data: function () {
+    return {
+      fileName: ''
+    };
+  },
+  computed: {
+    repoPath: function () {
       const fileName = this.fileName.trim().toLowerCase().replace(/\s+/g, '-');
 
-      this.filePath = `${ reposFolder }/${ fileName }`;
-    }
+      return `${ this.reposFolder }/${ fileName }`;
+    },
+    ...mapState(reposStore, [
+      'reposFolder'
+    ])
   }
 };
 </script>
@@ -54,8 +61,5 @@ export default {
 <style scoped>
 .new-repo {
   display: flex;
-}
-.repos-folder-input {
-  flex-grow: 1;
 }
 </style>
