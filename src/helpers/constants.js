a2c00892cb6e9a7683921780552e607a7b6b6d9e
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-02-16 09:53:57 -0500

    Diff tweaks

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,13 @@
+function dotDotDot () {
+  // &#x2009;
+  const thinSpace = ' ';
+  const interpunct = '·';
+  return [
+    interpunct,
+    interpunct,
+    interpunct
+  ].join(thinSpace);
+}
+
 export const APP_NAME = 'Sporkle!';
+export const DOT_DOT_DOT = dotDotDot();
