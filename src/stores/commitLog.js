236226d99b7ddcf2f4eb58c2ce9de821518d3c85
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-02-18 22:20:27 -0500

    Commit styles

--- src/stores/commitLog.js
+++ src/stores/commitLog.js
@@ -3,12 +3,28 @@ import { defineStore } from 'pinia';
 export const commitLogStore = defineStore('commitLog', {
   state: function () {
     return {
-      commitLogCollapsed: false
+      commitLogCollapsed: false,
+      hoveredCommitHash: '',
+      selectedCommitHash: ''
     };
   },
   actions: {
     toggleCommitLogCollapsed: function () {
       this.commitLogCollapsed = !this.commitLogCollapsed;
+    },
+    setHoveredCommitHash: function (hash) {
+      this.hoveredCommitHash = hash;
+    },
+    clearHoveredCommitHash: function (hash) {
+      if (this.hoveredCommitHash === hash) {
+        this.hoveredCommitHash = '';
+      }
+    },
+    setSelectedCommitHash: function (hash) {
+      this.selectedCommitHash = hash;
+    },
+    clearSelectedCommitHash: function (hash) {
+      this.selectedCommitHash = '';
     }
   }
 });
