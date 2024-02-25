b0bd9959e8017d2977e469214926b2917c0180eb
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-02-24 22:51:48 -0500

    basics working

--- src/stores/commitLog.js
+++ src/stores/commitLog.js
@@ -2,8 +2,6 @@ import { defineStore, mapState } from 'pinia';
 
 import { commitsStore } from '@/stores/commits.js';
 
-import { DEV_NULL } from '@/helpers/constants.js';
-
 export const commitLogStore = defineStore('commitLog', {
   state: function () {
     return {
@@ -35,20 +33,32 @@ export const commitLogStore = defineStore('commitLog', {
     ...mapState(commitsStore, [
       'commits'
     ]),
-    // This is the commit just before the selected commit
-    comparisonHash: function () {
+    selectedCommitIndex: function () {
       if (this.selectedCommitHash) {
         const commitIndex = this.commits.findIndex((commit) => {
           return commit.hash === this.selectedCommitHash;
         });
-        if (
-          commitIndex !== -1 &&
-          commitIndex !== (this.commits.length - 1)
-        ) {
-          return this.commits[commitIndex + 1]?.hash || DEV_NULL;
-        }
+        return commitIndex;
       }
-      return DEV_NULL;
+      return -1;
+    },
+    firstEverCommitSelected: function () {
+      const commitIndex = this.selectedCommitIndex;
+      return !!(
+        commitIndex > -1 &&
+        commitIndex === (this.commits.length - 1)
+      );
+    },
+    // This is the commit just before the selected commit
+    comparisonHash: function () {
+      const commitIndex = this.selectedCommitIndex;
+      if (
+        commitIndex > -1 &&
+        !this.firstEverCommitSelected
+      ) {
+        return this.commits[commitIndex + 1]?.hash || '';
+      }
+      return '';
     }
   }
 });
--- src/stores/fileDiffs.js
+++ src/stores/fileDiffs.js
@@ -10,6 +10,26 @@ import helpers from '@/helpers/index.js';
 
 const gitDiffParser = window.require('gitdiff-parser');
 
+function generateGitDiffCommandArray () {
+  return [
+    'git',
+    'diff',
+    // Look for any removed files that are 50% identical to any added files and assume it was renamed
+    '-M',
+    // do a more thorough search
+    '--find-copies-harder',
+    /**
+     * Could run:
+     * * git diff --shortstat --minimal
+     * * git diff --shortstat --patience
+     * * git diff --shortstat --histogram
+     * and compare them every time and use whatever is the smallest diff.
+     * Probably default to patience if all are the same.
+     */
+    '--histogram'
+  ];
+}
+
 export const fileDiffsStore = defineStore('fileDiffs', {
   state: function () {
     return {
@@ -58,38 +78,37 @@ export const fileDiffsStore = defineStore('fileDiffs', {
           return results.join('\n\n');
         });
     },
-    getNormalDiffs: async function () {
+    getUncommittedDiffs: function () {
+      const command = generateGitDiffCommandArray().join(' ');
+      return helpers.runCommand(command);
+    },
+    getCommitDiffs: async function () {
       let command = [
-        'git',
-        'diff',
-        // Look for any removed files that are 50% identical to any added files and assume it was renamed
-        '-M',
-        // do a more thorough search
-        '--find-copies-harder',
-        /**
-         * Could run:
-         * * git diff --shortstat --minimal
-         * * git diff --shortstat --patience
-         * * git diff --shortstat --histogram
-         * and compare them every time and use whatever is the smallest diff.
-         * Probably default to patience if all are the same.
-         */
-        '--histogram'
-      ];
-      if (this.selectedCommitHash) {
+        ...generateGitDiffCommandArray(),
         // The hash of the commit just before the one clicked
-        command.push(this.comparisonHash);
+        this.comparisonHash,
         // The hash of the commit clicked on in the commit log/timeline
-        command.push(this.selectedCommitHash);
-      }
-      command = command.join(' ');
+        this.selectedCommitHash
+      ].join(' ');
+      return helpers.runCommand(command);
+    },
+    getFirstCommitDiff: function () {
+      // git show works great for this simple use case, but git diff is better for the other commits
+      const command = [
+        'git',
+        'show',
+        this.selectedCommitHash
+      ].join(' ');
       return helpers.runCommand(command);
     },
     getDiffs: function () {
-      if (this.selectedCommitHash && this.comparisonHash === DEV_NULL) {
-        return this.getSpecificFileDiffsFromCommit();
+      if (this.firstEverCommitSelected) {
+        return this.getFirstCommitDiff();
       }
-      return this.getNormalDiffs();
+      if (this.selectedCommitHash) {
+        return this.getCommitDiffs();
+      }
+      return this.getUncommittedDiffs();
     },
     getAndParseDiffs: async function (currentRepoPath) {
       this.setFileDiffsLoading(true);
@@ -115,8 +134,9 @@ export const fileDiffsStore = defineStore('fileDiffs', {
       'commits'
     ]),
     ...mapState(commitLogStore, [
-      'selectedCommitHash',
-      'comparisonHash'
+      'comparisonHash',
+      'firstEverCommitSelected',
+      'selectedCommitHash'
     ])
   }
 });
